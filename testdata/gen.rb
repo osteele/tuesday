fdf37b8efbe367d0edd5d81f252adf538d26c639
Oliver Steele <steele@osteele.com>
2017-08-10 16:33:30 -0400

    Test table includes nanoseconds

--- testdata/gen.rb
+++ testdata/gen.rb
@@ -4,9 +4,10 @@
 # This is run manually rather than on go generate, so that go generate doesn't
 # require a Ruby installation.
 
-require "CSV"
+require 'CSV'
+require 'time'
 
-rt = Time.new(2006, 1, 2, 15, 4, 5, "-05:00")
+rt = Time.iso8601 "2006-01-02T15:04:05.123456789-05:00"
 CSV.open(File.join(File.dirname(__FILE__), "data.csv"), "w") do |csv|
     for mod in ['', '-', '_', '^', '#'] do
         for c in ('A'..'Z').to_a + ('a'..'z').to_a do
